4311aea2eaa893b10c341a14034a7db13637f843
Allan Benedito <allan.benedito@gmail.com>
2022-04-21 20:58:09 -0300

    Armazenando removendo e salvando no storage

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect } from 'react';
 import {Modal,TouchableWithoutFeedback, Keyboard, Alert} from 'react-native';
 
-import {Input} from '../../components/Forms/Input'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import uuid from 'react-native-uuid';
+
 import {InputForm} from '../../components/Forms/InputForm'
 import { useForm } from 'react-hook-form';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+import { useNavigation } from '@react-navigation/native';
 
 import {Button} from '../../components/Forms/Button'
 import { TransactionTypeButton } from '../../components/Forms/TransactionTypeButton';
@@ -27,6 +30,10 @@ interface FormData {
   [name: string]: any;
 }
 
+type NavigationProps = {
+  navigate:(screen:string) => void;
+}
+
 const schema = Yup.object().shape({
   name: Yup
   .string()
@@ -41,15 +48,21 @@ const schema = Yup.object().shape({
 
 export function Register(){
 
+    const dataKey = '@gofinances:transactions';
+
+
     const [category, setCategory] = useState({
       key: 'category',
       name: 'Categoria'
     });
 
+    const navigation = useNavigation<NavigationProps>();
+
     //foi criado para lidar com os inputs do formulário e importado da ferramenta hook form
     const {
       control,  // que servir para registrar os inputs em nosso formulário
       handleSubmit, // função que pega os valores do formulário e envia
+      reset, //reseta as informações digitadas pelo usuário. 
       formState: {errors} // serve para capturar os erros e melhorar a experiência do usuário
     } = useForm({
       resolver: yupResolver(schema) // vai fazer com que o envio dos valores dos enviados siga um padrão
@@ -71,7 +84,7 @@ export function Register(){
       setCategoryModalOpen(true);
     }
 
-    function handleRegister(form: FormData){
+    async function handleRegister(form: FormData){
 
       if(!transactionType)
       return Alert.alert('Selecione o tipo da transação');
@@ -79,14 +92,59 @@ export function Register(){
       if(category.key === 'category')
       return Alert.alert('Selecione a categoria');
 
-      const data = {
+      const newTransaction = {
+        id: String(uuid.v4()),
         name: form.name,
         amount: form.amount,
         transactionType,
-        category: category.key
+        category: category.key,
+        date: new Date()
+
       }
-      console.log('Log: data', data)
+      
+
+      try {
+        
+        const data = await AsyncStorage.getItem(dataKey);
+        const currentData = data ? JSON.parse(data) : [];
+
+        const dataFormatted = [
+          ...currentData,
+          newTransaction
+        ];
+
+        await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+
+        reset();
+        setTransactionType('');
+        setCategory({
+          key: 'category',
+          name: 'Categoria'
+        });
+  
+        navigation.navigate('Listagem');
+       
+      } catch (error) {
+        console.log(error);
+        Alert.alert("Não foi possível salvar");
+      }
     }
+
+    useEffect(() => {
+      async function loadData() {
+
+        const data = await AsyncStorage.getItem(dataKey); //GETiTEM serve para recuperar todos os dados 
+        console.log(JSON.parse(data!)) // o parse serve para converter o Json novamente em objeto
+                                       // O ! É UM RECURSO DO TYPESCRIPT QUE GARANTE QUE VAI RECEBER UM DADO NESSE "DATA" uma confiança
+      }
+      loadData();
+
+      //função para remover os store do celuar 
+      // async function removeAll() {
+      //   await AsyncStorage.removeItem(dataKey); 
+      // }
+      // removeAll();
+    },[]);
 
     return(
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
